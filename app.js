04c509949c22f6b914f8ab84dd5ac260778d190b
ankitaniket <ankitaniket3@gmail.com>
2023-03-27 21:13:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+
+let date = new Date().toJSON();
+// Set up storage for uploaded files
+const storage = multer.diskStorage({
+  destination: './public/uploads',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + date + path.extname(file.originalname));
+  }
+});
+
+// Initialize upload middleware
+const upload = multer({
+  storage: storage
+}).single('image');
+
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for handling image upload
+app.post('/employee', (req, res) => {
+  // Handle file upload
+  upload(req, res, (err) => {
+    if (err) {
+      // Handle upload error
+      console.error(err);
+      res.status(500).send({ error: 'An error occurred while uploading the image.' });
+    } else {
+      // Image uploaded successfully, send success response
+      res.status(200).send({ message: 'Image uploaded successfully.' });
+    }
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
---
+++ public/camera.js
@@ -0,0 +1,27 @@
+const video = document.getElementById('camera');
+      const canvas = document.getElementById('canvas');
+      const captureButton = document.getElementById('capture');
+      const constraints = { video: { width: 640, height: 480 } };
+
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+          video.srcObject = stream;
+        })
+        .catch((error) => {
+          console.error('Error accessing camera:', error);
+        });
+
+      captureButton.addEventListener('click', () => {
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+        canvas.toBlob((blob) => {
+          const formData = new FormData();
+          formData.append('image', blob, 'image.jpg');
+          fetch('/employee', { method: 'POST', body: formData })
+            .then(() => {
+              console.log('Image uploaded successfully');
+            })
+            .catch((error) => {
+              console.error('Error uploading image:', error);
+            });
+        }, 'image/jpeg', 0.9);
+      });
