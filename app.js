3e4fd061be852395855a2f084cff967b8eb1c51c
ankitaniket <ankitaniket3@gmail.com>
2023-03-29 19:18:33 +0530

    change port to 3000

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + date + path.extname(file.originalname));
   }
-
+  
 });
 
 // Initialize upload middleware
